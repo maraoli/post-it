9ee7fbfb39da85d6976021255ac9ba8c90c9f0e9
MARA TAMIRIS MIRANDA DE OLIVEIRA <43901448802@jpmorgan.sp>
2018-02-23 21:16:39 -0300

    webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  devServer:{
+      contentBase: './dist'
+  },
   devtool:'./src/index.js',
   output: {
     filename: 'bundle.js',
